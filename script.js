bdc2f04e07bc4d1e3704151eb3480a13d1f94df0
mamadouovo <mamadousanganaba@gmail.com>
2016-12-21 17:21:40 +0000

    homework

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 $(document).ready(function() {
-    $("body").keydown(function(e) {
+    var number = 0;
+    $("body").keydown(function(e) { //space Key
         if (e.keyCode == 32) {
 
-            $("body").css("background-color", "yellow");
+            $("body").css("background", "yellow");
+            console.log("image")
         }
-        if (e.keyCode == 69) {
-            $("body").append("🌈")
-            console.log("key");
+        if (e.keyCode == 69) { //key e
+            if (number < 5) {
+                $("body").append("🌈")
+                number++;
+                console.log(number);
+            }
         }
+
     });
-    $("h1").click(color = function() {
+
+    $("h1").click(function() {
         $("body").css("color", "#d9fcea");
     });
 });
